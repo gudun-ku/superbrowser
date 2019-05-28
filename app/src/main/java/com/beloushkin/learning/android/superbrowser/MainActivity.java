30c8299dce028b2e228cf13b43721a8b87861d91
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-05-28 22:41:23 +0400

    Initial commit - web view with back and forward.

---
+++ app/src/main/java/com/beloushkin/learning/android/superbrowser/MainActivity.java
@@ -0,0 +1,111 @@
+package com.beloushkin.learning.android.superbrowser;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.myProgressBar)
+    ProgressBar mProgressBar;
+    @BindView(R.id.myImageView)
+    ImageView mImageView;
+    @BindView(R.id.myWebView)
+    WebView mWebView;
+
+    private Toast mToast;
+
+    private void showMessage(String msgText) {
+        if (mToast != null)
+            mToast.cancel();
+
+        mToast = Toast.makeText(this,msgText,Toast.LENGTH_SHORT);
+        mToast.show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        mProgressBar.setMax(100);
+
+        mWebView.loadUrl("https://www.google.com");
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                mProgressBar.setProgress(newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                getSupportActionBar().setTitle(title);
+            }
+
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                super.onReceivedIcon(view, icon);
+                mImageView.setImageBitmap(icon);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.super_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_back:
+                onBackPressed();
+                break;
+            case R.id.menu_forward:
+                onForwardMenuPressed();
+                break;
+            case R.id.menu_refresh:
+                mWebView.reload();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onForwardMenuPressed() {
+        if (mWebView.canGoForward()) {
+            mWebView.goForward();
+        } else {
+            showMessage("Can't go further!");
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            finish();
+        }
+    }
+}
